3bd34557476895a0c6662ed96fe44ebbf63f128b
박한훈 <hhparkgoldenplanet.co.kr@baghanhun-ui-MacBookPro.local>
2024-06-04 15:22:11 +0900

    Update init

---
+++ js/script.js
@@ -0,0 +1,47 @@
+/*=====페이지 이동 시작=====*/
+// 홈
+function moveMainPage() {
+    console.log("moveMainPage()");
+    window.location.href = "/index.html";
+}
+// 네비게이션
+function moveMypage() {
+    console.log("moveMypage()");
+    window.location.href = "/mypage/mypage.html";
+}
+function moveCart() {
+    console.log("moveCart()");
+    window.location.href = "/cart/cart.html";
+}
+// product
+function moveProductItemList() {
+    console.log("moveProductItemList()");
+    window.location.href = "/product/productItemList.html";
+}
+function moveProductItemDetail() {
+    console.log("moveProductItemDetail()");
+    window.location.href = "/product/productItemDetail.html";
+}
+// product/order
+function moveProductCheckout() {
+    console.log("moveProductItemList()");
+    window.location.href = "/product/order/productCheckout.html";
+}
+function moveProductPurchase() {
+    console.log("moveProductItemDetail()");
+    window.location.href = "/product/order/productPurchase.html";
+}
+// product/history
+function moveProductHistory() {
+    console.log("moveMypageProductList()");
+    window.location.href = "/product/history/productHistory.html";
+}
+function moveProductHistoryDetail() {
+    console.log("moveMypageProductDetail()");
+    window.location.href = "/product/history/productHistoryDetail.html";
+}
+function moveProductHistoryRefund() {
+    console.log("moveMypageProductRefund()");
+    window.location.href = "/product/history/productRefund.html";
+}
+/*=====페이지 이동 끝=====*/
