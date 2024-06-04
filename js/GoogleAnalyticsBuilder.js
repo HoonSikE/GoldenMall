e780e304d2ca671c3f19b7efb1402871e5484b76
박한훈 <hhparkgoldenplanet.co.kr@baghanhun-ui-MacBookPro.local>
2024-06-04 16:19:20 +0900

    Update PageView Test

--- js/GoogleAnalyticsBuilder.js
+++ js/GoogleAnalyticsBuilder.js
@@ -68,7 +68,7 @@ function sendGAPage(object) {
       new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
       j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
       'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-WPC9HDZQ');
+      })(window,document,'script','dataLayer','GTM-WN78NTLT');
       // End Google Tag Manager
     // }
   } catch (e) {
